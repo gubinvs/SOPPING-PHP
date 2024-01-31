eb65788e23837062057eefa8a3f3fee7f777a653
master <gubinvs@gmail.com>
2024-01-31 20:44:17 +0300

    s

--- src/php/class/classNomenclature.php
+++ src/php/class/classNomenclature.php
@@ -8,6 +8,15 @@ class Nomenclature {
     public $brend;  //-- Бренд
     public $type;  //-- Тип номенклатуры
 
+    function __addNomenclature ($brend, $type) {
+        $this->$brend = $brend;
+        $this->$type = $type;
+    }
+
+    function __printNomenclature () {
+        echo $this->brend;
+        echo $this->type;
+    }
 
 }
 
