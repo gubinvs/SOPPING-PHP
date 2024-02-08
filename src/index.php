94493105114a6fb3c715215ec364893a9c578e9f
master <gubinvs@gmail.com>
2024-02-08 19:05:19 +0300

    ч

--- src/index.php
+++ src/index.php
@@ -3,26 +3,27 @@
 <?php
 $_titlePage = "Компонент энергии - поставки комплектующих для средств автоматизации";
 $_headDescr = "Комплексные поставки комплектующих для средств автоматизации";
-$_canonical = "dg";
+$_canonical = "Прописать требования яндекса";
 require_once 'php/modules/head.php';
 require_once 'php/class/connector.php';
+// Подключение к базе данных с пользователями для проверки и авторизации
+$conn = new Connector();
+$conn->ConnectDbUser();
 ?>
 
 <body>
     <!--Подключение header-->
     <?php
     require_once "php/modules/header.php";
+    
     ?>
 
     <!--Основная секция страницы-->
     <main>
         <h1 class="visually-hidden h1-visually h1__visually" style="visibility: hidden;">Комплектующие для средств автоматизации у нас на сайте</h1>
         <div class="container">
-            <span>Подключение к базе данных:</span>
             <?php
-            // Подключение к базе данных с пользователями для проверки и авторизации
-                $conn = new Connector();
-                $conn->ConnectDbUser();
+ 
             ?>
 
         </div>
