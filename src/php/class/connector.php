bb25c298b1173baf258307c053efd4a2b68d05f7
master <gubinvs@gmail.com>
2024-02-09 09:04:35 +0300

    ва

--- src/php/class/connector.php
+++ src/php/class/connector.php
@@ -1,8 +1,8 @@
-<?php 
+<?php
+// подключение к базе MySQL
     class Connector {
-    // подключение к базе MySQL
-
-        // подключение к базе данных с пользователями сервиса
+    
+        // подключение к базе данных содержащей данные пользователей сервиса, для проверки авторизации
         function ConnectDbUser () {
             // Функция возвращает строку подключения к базе данных и собственно ее подключает
             return $connect = new PDO("mysql:host=gubinv.beget.tech;port=3306;dbname=gubinv_userecomp", "gubinv_userecomp", "*5syxsFi");
