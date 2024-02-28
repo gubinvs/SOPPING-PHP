448efc38bd53f3d3632bd7682e908741acfc880f
Vladimir <gubinvs@gmail.com>
2024-02-28 10:03:33 +0300

    a

--- src/index.php
+++ src/index.php
@@ -5,14 +5,12 @@ $_titlePage = "Компонент энергии - поставки компле
 $_headDescr = "Комплексные поставки комплектующих для средств автоматизации";
 $_canonical = "Прописать требования яндекса";
 require_once 'php/modules/head.php';
-require_once 'php/class/connector.php';
 ?>
 
 <body>
     <!--Подключение header-->
     <?php
-    require_once "php/modules/header.php";
-    
+        require_once "php/modules/header.php";
     ?>
 
     <!--Основная секция страницы-->
@@ -20,8 +18,22 @@ require_once 'php/class/connector.php';
         <h1 class="visually-hidden h1-visually h1__visually" style="visibility: hidden;">Комплектующие для средств автоматизации у нас на сайте</h1>
         <div class="container">
             <?php
-
+                
             ?>
+        
+    
+    
+        <form method="post" action="do_register.php">
+            <div class="mb-3">
+                <label for="username" class="form-label">Username</label>
+                <input type="text" class="form-control" id="username" name="username" required>
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password" name="password" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Register</button>
+        </form>
         </div>
     </main>
     <!--Подключение скриптов JS-->
