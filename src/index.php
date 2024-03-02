0ce9f46b765f145ff18db659f82060023eaf466e
master <gubinvs@gmail.com>
2024-03-02 17:23:05 +0300

    a

--- src/index.php
+++ src/index.php
@@ -11,7 +11,7 @@ require_once 'php/class/connectorMSQL.php';
 <body>
     <!--Подключение header-->
     <?php
-        require_once "php/modules/header.php";
+    require_once "php/modules/header.php";
 
     ?>
 
@@ -19,20 +19,22 @@ require_once 'php/class/connectorMSQL.php';
     <main>
         <h1 class="visually-hidden h1-visually h1__visually" style="visibility: hidden;">Комплектующие для средств автоматизации у нас на сайте</h1>
         <div class="container">
-                <form method="post" action="php/class/go_register.php">
-                    <div class="mb-3">
-                        <label for="username" class="form-label">Имя пользователя</label>
-                        <input type="text" class="form-control" id="username" name="username" required>
-                    </div>
-                    <div class="mb-3">
-                        <label for="password" class="form-label">Пароль</label>
-                        <input type="password" class="form-control" id="password" name="password" required>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Регистрация</button>
-                </form>
+            <form method="POST" action="php/class/go_register.php">
+                <div class="mb-3">
+                    <label for="username" class="form-label">Имя пользователя</label>
+                    <input type="text" class="form-control" id="username" name="username" required>
+                </div>
+                <div class="mb-3">
+                    <label for="password" class="form-label">Пароль</label>
+                    <input type="password" class="form-control" id="password" name="password" required>
+                </div>
+                <button type="submit" class="btn btn-primary">Регистрация</button>
+                <input type="hidden" name="act" value="order">
+            </form>
         </div>
     </main>
     <!--Подключение скриптов JS-->
     <script src="js/app.js" type="module"></script>
 </body>
+
 </html>
--- src/php/class/go_register.php
+++ src/php/class/go_register.php
@@ -6,21 +6,29 @@ try {
     // Подключение к базе данных с параметрами пользователя
     $con = new ConnectorMSQL();
     $db = $con->ConnectDbUser();
-    if ($con) {
-        echo 'Все в порядке, подключились и работаем!';
-    } else {
-        echo 'Нет подключения к базе данных';
-    }
+        if (!$con) {
+            echo 'Нет подключения к базе данных';
+        }
     } catch (PDOException $e) {
         echo "Database error: " . $e->getMessage();
         echo "Ошибка подключения базы данных файл go_register.php";
     }
 
-    // Формирование запроса в базу данных
-    $sql = 'INSERT INTO `userSystems`(`userEmail`, `userPassword`) VALUES ("gubinvs@gmail.com", "qwerty")';
-    
+    // Приняли данные (методом post) инициализируем и присваиваем значение переменным данными из сообщения.
+    if ($_POST['act'] == 'order') {
+        $userName = ($_POST['username']);
+        $userPassword = ($_POST['password']);
+    }
 
+    // Формирование запроса в базу данных
+    $sql = "INSERT INTO `userSystems`(`userEmail`, `userPassword`) VALUES ( '$userName', '$userPassword' )";
+    // echo "Запрос: " . $sql;
 
     // Передача запроса в базу данных
     $db->exec($sql);
+    // echo "Добавлен в базу данных";
+    
+    // Отключение от базы данных
+    $con = null;
+    // echo "<br>" . "Базу отключил!"
 ?>
