114a806a92142cd2ba9b444c248ea3d5eb5c2cb3
master <gubinvs@gmail.com>
2024-03-02 15:06:00 +0300

    z

--- src/php/class/brandList.php
+++ src/php/class/brandList.php
@@ -1,14 +1,12 @@
 <?php 
-include "brand.php";
-//-- Перечень актуальной номенклатуры товаров
-$product = [
-    new Brand("Schneider", ["Розетки-Schneider", "Выключатели-Schneider", "Контроллеры-Schneider", "Контакторы-Schneider"]),
-    new Brand("ABB", ["Розетки-ABB", "Выключатели-ABB", "Контроллеры-ABB", "Контакторы-ABB"]),
-    new Brand("Siemens", ["Розетки-Siemens", "Выключатели-Siemens", "Контроллеры-Siemens", "Контакторы-Schneider"]),
-    new Brand("Phoenix", ["Розетки-Phoenix", "Выключатели-Phoenix", "Контроллеры-Phoenix", "Контакторы-Phoenix"])
-];
-
-//-- Метод определяет
+    include "brand.php";
+    //-- Перечень актуальной номенклатуры товаров
+    $product = [
+        new Brand("Schneider", ["Розетки-Schneider", "Выключатели-Schneider", "Контроллеры-Schneider", "Контакторы-Schneider"]),
+        new Brand("ABB", ["Розетки-ABB", "Выключатели-ABB", "Контроллеры-ABB", "Контакторы-ABB"]),
+        new Brand("Siemens", ["Розетки-Siemens", "Выключатели-Siemens", "Контроллеры-Siemens", "Контакторы-Schneider"]),
+        new Brand("Phoenix", ["Розетки-Phoenix", "Выключатели-Phoenix", "Контроллеры-Phoenix", "Контакторы-Phoenix"])
+    ];
 ?>
 
 
